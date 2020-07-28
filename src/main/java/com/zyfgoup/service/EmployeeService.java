202558840d72b5559ace3bb2f308337ca901e7b2
Zyfgoup <619122012@qq.com>
2020-07-28 17:54:19 +0800

    员工模块

---
+++ src/main/java/com/zyfgoup/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.zyfgoup.service;
+
+import com.zyfgoup.entity.Employee;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zyfgoup
+ * @since 2020-07-28
+ */
+public interface EmployeeService extends IService<Employee> {
+
+}
